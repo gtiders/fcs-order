5407356923f3f22bee7a05f23b77e46cb7786f33
gtiders <gtiders@qq.com>
2025-10-30 23:56:13 +0800

    1

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ import os
 import spglib
 from setuptools import setup, find_packages
 from setuptools.extension import Extension
+from Cython.Build import cythonize
 
 # Auto-detect spglib paths for pip installation
 spglib_dir = os.path.dirname(spglib.__file__)
@@ -13,32 +14,24 @@ print(f"spglib_dir: {spglib_dir}")
 INCLUDE_DIRS = [spglib_dir]
 LIBRARY_DIRS = [os.path.join(spglib_dir, "lib64")]
 
-# Set USE_CYTHON to True if you want include the cythonization in your build process.
-USE_CYTHON = True
-
-ext = ".pyx" if USE_CYTHON else ".c"
-
 extensions = [
     Extension(
         "fcs_order.thirdorder.thirdorder_core",
-        ["src/fcs_order/thirdorder/thirdorder_core" + ext],
+        ["src/fcs_order/thirdorder/thirdorder_core" + "pyx"],
         include_dirs=[numpy.get_include(), "src/fcs_order/thirdorder"] + INCLUDE_DIRS,
         library_dirs=LIBRARY_DIRS,
         libraries=["symspg"],
     ),
     Extension(
         "fcs_order.fourthorder.fourthorder_core",
-        ["src/fcs_order/fourthorder/fourthorder_core" + ext],
+        ["src/fcs_order/fourthorder/fourthorder_core" + "pyx"],
         include_dirs=[numpy.get_include(), "src/fcs_order/fourthorder"] + INCLUDE_DIRS,
         library_dirs=LIBRARY_DIRS,
         libraries=["symspg"],
     ),
 ]
 
-if USE_CYTHON:
-    from Cython.Build import cythonize
-
-    extensions = cythonize(extensions)
+extensions = cythonize(extensions)
 
 setup(
     name="fcs-order",
@@ -46,5 +39,5 @@ setup(
     packages=find_packages(where="src"),
     package_dir={"": "src"},
     include_package_data=True,
-    python_requires=">=3.7",
+    python_requires=">=3.9",
 )
