ceb44f06971f3d52b5a0f0ab4e1ec8ce3859444a
gtiders <gtiders@qq.com>
2026-08-03 21:39:55 +0800

    Defer reference-only potential import

--- tests/reference/phono3py/K4As4Pt2_FC23/test_potential.py
+++ tests/reference/phono3py/K4As4Pt2_FC23/test_potential.py
@@ -2,7 +2,6 @@ from __future__ import annotations
 
 import numpy as np
 import pytest
-from pypolymlp.calculator.utils.ase_calculator import PolymlpASECalculator
 
 from tests.reference.phono3py.K4As4Pt2_FC23.case import (
     DATA,
@@ -12,6 +11,8 @@ from tests.reference.phono3py.K4As4Pt2_FC23.case import (
 
 @pytest.mark.reference
 def test_packaged_potential_reproduces_first_MLFCS_force_configuration():
+    from pypolymlp.calculator.utils.ase_calculator import PolymlpASECalculator
+
     data, calculation, _ = calculation_and_reference(2)
     atoms = calculation.sow()[0]
     atoms.calc = PolymlpASECalculator(pot=DATA / "polymlp.yaml")
