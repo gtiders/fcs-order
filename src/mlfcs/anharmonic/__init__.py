3b1c1203f6329468677f01c6e7efc5a2521eabd7
gtiders <gtiders@qq.com>
2026-08-18 16:28:38 +0800

    Remove stale anharmonic compatibility hook

--- src/mlfcs/anharmonic/__init__.py
+++ src/mlfcs/anharmonic/__init__.py
@@ -2,20 +2,4 @@
 
 from mlfcs.anharmonic.scph import LoopSCPH, LoopSCPHResult, harmonic_frequencies
 
-__all__ = [
-    "SSCHA",
-    "EnsembleDiagnostics",
-    "HarmonicEnsemble",
-    "LoopSCPH",
-    "LoopSCPHResult",
-    "SSCHAIteration",
-    "harmonic_frequencies",
-]
-
-
-def __getattr__(name: str):
-    if name in {"SSCHA", "SSCHAIteration", "HarmonicEnsemble", "EnsembleDiagnostics"}:
-        from mlfcs.anharmonic import sscha
-
-        return getattr(sscha, name)
-    raise AttributeError(name)
+__all__ = ["LoopSCPH", "LoopSCPHResult", "harmonic_frequencies"]
