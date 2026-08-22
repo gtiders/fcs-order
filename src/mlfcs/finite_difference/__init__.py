ce602adebce69d0015d3d49f574379294077c092
gtiders <gtiders@qq.com>
2026-08-22 13:52:39 +0800

    Revert "feat: add finite-difference extrapolation backend"
    This reverts commit 80619fc30de8d621a44e8348b5ffb3165c28948e.

--- src/mlfcs/finite_difference/__init__.py
+++ src/mlfcs/finite_difference/__init__.py
@@ -1,6 +1,5 @@
 """Recursive finite-difference stencils and displacement plans."""
 
-from mlfcs.finite_difference.extrapolation import ExtrapolationBackend
 from mlfcs.finite_difference.stencil import CentralDifferenceStencil
 
-__all__ = ["CentralDifferenceStencil", "ExtrapolationBackend"]
+__all__ = ["CentralDifferenceStencil"]
