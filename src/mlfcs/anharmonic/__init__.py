7f58c4f602553585358d0f9e38e73cbf4c183afa
gtiders <gtiders@qq.com>
2026-08-22 13:52:36 +0800

    Revert "Remove stale anharmonic compatibility hook"
    This reverts commit 3b1c1203f6329468677f01c6e7efc5a2521eabd7.

--- src/mlfcs/anharmonic/__init__.py
+++ src/mlfcs/anharmonic/__init__.py
@@ -2,4 +2,20 @@
 
 from mlfcs.anharmonic.scph import LoopSCPH, LoopSCPHResult, harmonic_frequencies
 
-__all__ = ["LoopSCPH", "LoopSCPHResult", "harmonic_frequencies"]
+__all__ = [
+    "SSCHA",
+    "EnsembleDiagnostics",
+    "HarmonicEnsemble",
+    "LoopSCPH",
+    "LoopSCPHResult",
+    "SSCHAIteration",
+    "harmonic_frequencies",
+]
+
+
+def __getattr__(name: str):
+    if name in {"SSCHA", "SSCHAIteration", "HarmonicEnsemble", "EnsembleDiagnostics"}:
+        from mlfcs.anharmonic import sscha
+
+        return getattr(sscha, name)
+    raise AttributeError(name)
